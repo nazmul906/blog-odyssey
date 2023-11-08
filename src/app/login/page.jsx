2e194593208f57275d03fe8b0b4aeca0fb5212c5
nazmul906 <nazmulhb96@gmail.com>
2023-11-09 00:05:44 +0600

    login page is added

---
+++ src/app/login/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
+import styles from "./login.module.css";
+import { useRouter } from "next/navigation";
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
+        <div className={styles.socialButton}>Sign in with Github</div>
+        <div className={styles.socialButton}>Sign in with Facebook</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
