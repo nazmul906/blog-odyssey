474f1862cdba709afa810b426cf2b2fbffa2f032
nazmul906 <nazmulhb96@gmail.com>
2023-11-10 01:22:46 +0600

    prisma provider bug is fixed and user login data is asuccessfully stored in mongo db

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,10 +1,12 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 // implementing oAuth for login via google
+
+// prisma adapter will handle our user accounts and token automatically
+// so if u login by different provider(ggl/git) but with the same email,u dnt have to create two diffrernt user ,adapter handle the one account for single email
 export const authOptions = {
-  // prisma adapter will handle our user accounts and token automatically
-  // so if u login by different provider(ggl/git) but with the same email,u dnt have to create two diffrernt user ,adapter handle the one account for single email
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
